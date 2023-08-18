22611505e150251b3971475f61592ab741ab1b5d
tamasandor <tamas.andor@yahoo.com>
2023-08-18 23:44:12 +0200

    Solved until ex04 (included)

---
+++ Piscine/Code/c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/18 18:08:47 by atamas            #+#    #+#             */
+/*   Updated: 2023/08/18 18:09:57 by atamas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+}
