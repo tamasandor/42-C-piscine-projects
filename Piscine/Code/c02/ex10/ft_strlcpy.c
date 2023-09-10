9367a26a7b942d137ddcff4c4fcd132171ded29e
tamasandor <tamas.andor@yahoo.com>
2023-09-10 23:09:22 +0200

    Finished c02

---
+++ Piscine/Code/c02/ex10/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 23:09:58 by atamas            #+#    #+#             */
+/*   Updated: 2023/09/06 23:51:46 by atamas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned	int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	returnnum;
+	unsigned int	i;
+
+	returnnum = 0;
+	i = 0;
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	while (src[returnnum] != '\0')
+	{
+		returnnum++;
+	}
+	return (returnnum);
+}
